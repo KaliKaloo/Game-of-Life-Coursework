956a84edf7c09a843869c910b3973b67b6cc3b2a
kalikaloo <pragyagurung002@gmail.com>
2020-12-04 22:31:47 +0000

    working

--- Concurrent/benchmark_test.go
+++ Concurrent/benchmark_test.go
@@ -9,16 +9,28 @@ import (
 )
 
 const turnNum = 1000
+
 var value bool
 
 func Benchmark(b *testing.B) {
 	tests := []gol.Params{
 		{ImageWidth: 512, ImageHeight: 512, Threads: 1},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 2},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 3},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 4},
 		{ImageWidth: 512, ImageHeight: 512, Threads: 5},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 6},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 7},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 8},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 9},
 		{ImageWidth: 512, ImageHeight: 512, Threads: 10},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 11},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 12},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 13},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 14},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 15},
 		{ImageWidth: 512, ImageHeight: 512, Threads: 16},
 	}
-
 	for _, t := range tests {
 		value = true
 		os.Stdout = nil
